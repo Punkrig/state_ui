8d7a4729359c225e3a7b1a635d0bf73124dffc83
Punkrig <igorbrandaao@gmail.com>
2024-06-07 12:35:06 -0300

    FEAT:menu responsivo. -Adição do menu responsivo;

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,11 @@
+import { useState } from "react";
 import "./navbar.scss"
 function Navbar(){
+    const [open, setOpen]= useState(false)
    return( 
         <nav>
             <div className="left">
-                <a href="/">
+                <a href="/" className="logo">
                     <img src="/logo.png" alt="imagem da logo" />
                     <span>LamaState</span>
                 </a>
@@ -14,7 +16,24 @@ function Navbar(){
             </div>
             <div className="rigth">
                 <a href="/">Sing in</a>
-                <a href="/" className="register">Sing up</a>
+                <a href="/" className="register">
+                    Sing up
+                </a>
+                <div className="menuIcon">
+                    <img 
+                    src="/menu.png" 
+                    alt="figura do menu" 
+                    onClick={()=>setOpen(prev=>!prev)}
+                    />
+                </div>
+                <div className={open ? "menu active": "menu"}>
+                    <a href="/">Home</a>
+                    <a href="/">About</a>
+                    <a href="/">Contact</a>
+                    <a href="/">Agents</a>
+                    <a href="/">Sing in</a>
+                    <a href="/">Sing up</a>
+                </div>
             </div>
         </nav>
     )
